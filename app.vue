b4da7827b3e04bc3c968c26bcc0fb7ace9c8361e
abdullahkabakk <05200000101@ogrenci.ege.edu.tr>
2022-09-02 21:25:20 +0300

    Added Sign in & Sign out

--- app.vue
+++ app.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <button class="button" @click="signIn">Sign In</button>
+    <button class="button" @click="signOut">Sign Out</button>
   </div>
 </template>
+
+<script setup>
+import {createUser, signInUser, signOutUser} from "./composables/useFirebase";
+
+const signIn = async () => {
+  const email = "abdulla@email.com"
+  const password = "12345678"
+  const credentials = await signInUser(email, password)
+  console.log(credentials)
+}
+
+const signOut =  async () => {
+  const result = await signOutUser();
+  console.log("result:", result)
+}
+
+onMounted(async () => {
+
+
+  // const email = "abdulla@email.com"
+  // const password = "12345678"
+  // const credentials = await createUser(email, password)
+  // console.log(credentials)
+
+})
+</script>
+
+<style>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+</style>
